485b36626da75da11c810baaac14370028fe1ef3
lee-tae-eon <mktears0609@gmail.com>
2021-05-28 01:31:28 +0900

    learn class

---
+++ ES6/class.js
@@ -0,0 +1,34 @@
+class Counter {
+  constructor({ initialNumber = 0, counterId, plusId, minusId }) {
+    this.count = initialNumber;
+    this.counter = document.getElementById(counterId);
+    this.counter.innerHTML = initialNumber;
+    this.plusBtn = document.getElementById(plusId);
+    this.minusBtn = document.getElementById(minusId);
+    this.addEventListners();
+  }
+  addEventListners = () => {
+    console.log(this);
+    this.plusBtn.addEventListener("click", this.increase);
+    this.minusBtn.addEventListener("click", this.decrease);
+  };
+  repaintCount = () => {
+    this.counter.innerText = this.count;
+  };
+  increase = () => {
+    this.count = this.count + 1;
+    this.repaintCount();
+  };
+  decrease = () => {
+    this.count = this.count - 1;
+    this.repaintCount();
+  };
+}
+
+new Counter({ counterId: "count", plusId: "add", minusId: "minus" });
+new Counter({
+  initialNumber: 100,
+  counterId: "count2",
+  plusId: "add2",
+  minusId: "minus2",
+});
---
+++ ES6/literal.js
@@ -0,0 +1,52 @@
+//-------------ES& literal html fragment ---------------
+
+// const wrapper = document.querySelector(".wrapper");
+
+// const addWelcome = () => {
+// 기존 엘레멘트 추가방식
+// const div = document.createElement("div");
+// const h1 = document.createElement("h1");
+// h1.innerText = "hello";
+// div.append(h1);
+
+// es6 literal - 백틱은 줄바꿈도 인식
+//   const div = `
+//     <div class= "hello">
+//       <h1 class="title">hello</h1>
+//     </div>
+//   `;
+//   wrapper.innerHTML = div;
+// };
+
+// setTimeout(addWelcome, 2000);
+
+// const friends = ["me", "lynn", "lee", "woo"];
+
+// const list = `
+//   <h1>People i love</h1>
+//   <ul>
+//     ${friends.map((friend) => `<li>${friend}</li>`).join(" ")}
+//   </ul>
+// `;
+// wrapper.innerHTML = list;
+
+//-----------ES6 literal Cloning styled component----------
+// styled components is package or library for react
+// styled components는 js에서 css를 쓸수 있고 html을 얻을 수 있다.
+// ``를 이용해서 만들어본다.
+
+const styled = (aElement) => {
+  const el = document.createElement(aElement);
+  return el;
+};
+
+const title = styled("h1")(`
+  border-radius: 10x;
+  color:blue;
+`);
+console.log(title);
+
+// styled("h1")`
+//   border-radius: 10px;
+//   color: blue;
+// `;
